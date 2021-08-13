467f0e308d0cb9d7ece9214855cd96b5ea937bc6
criscandeloro <criscandeloro@gmail.com>
2021-08-13 20:55:19 -0300

    Criei o teste para incluir um pet na API Rest aula 3

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -12,6 +12,7 @@ import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.is;
 
 // 3 - Classe
 public class Pet {
@@ -40,6 +41,8 @@ public class Pet {
         .then()
                 .log().all()
                 .statusCode(200)
+                .body("name",is ("Spoke"))
+                .body("status", is("available"))
         ;
     }
 
